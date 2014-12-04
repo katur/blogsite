df615ef1e1195bf1e10686459cd032bf8c345872
Katherine Erickson <katherine.erickson@gmail.com>
2014-12-03 21:56:56 -0500

    More deploying debugging

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -90,6 +90,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
