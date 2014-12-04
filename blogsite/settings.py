f25c3df4109a092f638b4f6884e7f445ff355e96
Katherine Erickson <katherine.erickson@gmail.com>
2014-12-04 18:07:45 -0500

    Remove outdated staticfile location.

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -89,7 +89,3 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
