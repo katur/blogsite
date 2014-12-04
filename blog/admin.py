396c54ab76700af0398554ffe9d8a50978a6dcf6
Katherine Erickson <katherine.erickson@gmail.com>
2014-12-04 18:17:00 -0500

    set up Post and Blog admin, for moderation purposes

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog, Post
+
+
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Post, PostAdmin)
