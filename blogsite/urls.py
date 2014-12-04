b6c7e7d15384fc46f11eba1b1bd8b2742c245d53
Katherine Erickson <katherine.erickson@gmail.com>
2014-12-04 18:05:05 -0500

    enable admin for moderation tasks, such as creating blogs and adding contributors

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import patterns, include, url
-# from django.contrib import admin
+from django.contrib import admin
 
 urlpatterns = patterns(
     '',
     url(r'', include('website.urls')),
     url(r'^blog/', include('blog.urls')),
-    # Examples:
-    # url(r'^$', 'blogsite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
