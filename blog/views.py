94f91b66ce0c176518fff80adcaddd8ddc637cde
Katherine Erickson <katherine.erickson@gmail.com>
2014-12-05 22:51:39 -0500

    Revert to more naive solution for tag cloud, to accommodate single blog.
    Can't do nested reverse lookup in Django to get from tags to posts to blog. So
    instead of using aggregate queries, for now, do a very naive solution of just
    iterating over all tags to count them.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from math import log
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.db.models import Count, Max, Min
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
@@ -9,7 +8,7 @@ from blogsite.settings import (BLOG_POSTS_PER_PAGE,
                                BLOG_POST_TRUNCATION_FACTOR)
 from blog.models import Blog, Post, PostView
 
-from taggit.models import Tag
+from taggit.models import TaggedItem
 
 
 def blogs(request):
@@ -22,6 +21,7 @@ def blogs(request):
 def blog(request, blog_slug):
     blog = get_object_or_404(Blog, slug=blog_slug)
     all_posts = Post.objects.filter(blog=blog)
+    tag_cloud = get_tag_cloud(blog=blog)
 
     paginator = Paginator(all_posts, BLOG_POSTS_PER_PAGE)
     page = request.GET.get('page')
@@ -39,6 +39,7 @@ def blog(request, blog_slug):
         'blog': blog,
         'posts': display_posts,
         'truncation_factor': BLOG_POST_TRUNCATION_FACTOR,
+        'tag_cloud': tag_cloud,
     }
 
     return render(request, 'blog.html', context)
@@ -67,7 +68,7 @@ def record_post_view(request, post):
         post_view.save()
 
 
-def get_tag_cloud(count_threshold=0, max_size=1.75, min_size=.75):
+def get_tag_cloud(count_threshold=0, max_size=1.75, min_size=.75, blog=None):
     """Get the tags along with their relative sizes for 'tag cloud'.
 
     Tags are only displayed if their count is above threshold.
@@ -76,19 +77,39 @@ def get_tag_cloud(count_threshold=0, max_size=1.75, min_size=.75):
 
     The math is by http://dburke.info/blog/logarithmic-tag-clouds/
     """
-    tags_with_counts = Tag.objects.annotate(
-        count=Count('taggit_taggeditem_items'))
-    min_count = tags_with_counts.aggregate(Min('count')).values()[0]
-    max_count = tags_with_counts.aggregate(Max('count')).values()[0]
+    def tally(tag, tag_with_counts):
+        if tag not in tags_with_counts:
+            tags_with_counts[tag] = 0
+        tags_with_counts[tag] += 1
+
+    tags_with_counts = {}
+    tag_cloud = []
+
+    if blog:
+        posts = Post.objects.filter(blog=blog)
+        for post in posts:
+            for tag in post.tags.all():
+                tally(tag, tags_with_counts)
+
+    else:
+        tagged_items = TaggedItem.objects.all()
+        for tagged_item in tagged_items:
+            tally(tagged_item.tag, tags_with_counts)
+
+    if not tags_with_counts:
+        return tag_cloud
+
+    min_count = min(tags_with_counts.itervalues())
+    max_count = max(tags_with_counts.itervalues())
     constant = log(max_count - (min_count-1))/(max_size-min_size or 1)
 
-    tag_cloud = []
-    for tag in tags_with_counts:
-        if tag.count >= count_threshold:
-            size = log(tag.count - (min_count-1))/constant + min_size
+    for tag, count in tags_with_counts.iteritems():
+        if count >= count_threshold:
+            size = log(count - (min_count-1))/constant + min_size
             tag_cloud.append({
                 'tag': tag,
-                'count': tag.count,
+                'count': count,
                 'size': round(size, 7)
             })
+
     return tag_cloud
