b1331f54adf6985d7a191855d0f4c842176a99e2
Katherine Erickson <katherine.erickson@gmail.com>
2014-12-08 15:08:12 -0500

    Add custom login/logout page

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -101,3 +101,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.messages.context_processors.messages",
     "django.core.context_processors.request"
 )
+
+# Authentication
+LOGIN_URL = 'login_url'
+LOGIN_REDIRECT_URL = 'home_url'
--- website/urls.py
+++ website/urls.py
@@ -5,3 +5,13 @@ urlpatterns = patterns(
     'website.views',
     url(r'^$', 'home', name='home_url'),
 )
+
+urlpatterns += patterns(
+    '',
+    url(
+        r'^login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'login.html'}, name='login_url'),
+    url(
+        r'^logout/$', 'django.contrib.auth.views.logout',
+        {'next_page': '/'}, name='logout_url'),
+)
